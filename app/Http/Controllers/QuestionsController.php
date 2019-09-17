32541b86f9aa16b4b444762681d9eb22504cd1af
Jean-Luc <jlpetelot@madinci.org>
2019-09-17 16:34:33 +0200

    Implement authorization using Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
+
+    /**
+     * Constructeur pour appeler un middleware pour interdire l'accès aux non loggués à tout sauf aux méthodes index () et show ()
+     *
+     * @return void
+    */
+    public function __construct ()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -60,6 +71,7 @@ class QuestionsController extends Controller
      */
     public function edit (Question $question)
     {
+        $this->authorize("update", $question);
         return view('questions.edit', compact('question'));
     }
     /**
@@ -71,8 +83,10 @@ class QuestionsController extends Controller
      */
     public function update (AskQuestionRequest $request, Question $question)
     {
+        $this->authorize("update", $question);
+    
         $question->update($request->only('title', 'body'));
-
+    
         return redirect()->route('questions.index')->with('success', "votre question a bien été mise à jour !");
     }
     /**
@@ -83,8 +97,9 @@ class QuestionsController extends Controller
      */
     public function destroy (Question $question)
     {
+        $this->authorize("delete", $question);
+    
         $question->delete();
-
-        return redirect('/questions')->with('success', "votre question dont le titre était : « $question->title » a bien été supprimée !");
+        return redirect()->route('questions.index')->with('success', "votre question a bien été supprimée !");
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -35,17 +35,21 @@
                                     <div class="d-flex align-items-center">
                                         <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                         <div class="ml-auto">
-                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                            <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Êtes-vous sûr ?')">Delete</button>
-                                            </form>
+                                            @can ('update', $question)
+                                                <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            @endcan
+                                            @can ('delete', $question)
+                                                <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick='return confirm("Êtes-vous sûr ?")'>Delete</button>
+                                                </form>
+                                            @endcan
                                         </div>
                                     </div>
                                     <p class="lead">
                                         Asked by
-                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}< /a>
                                         <small class="text-muted">{{ $question->created_date }}</small>
                                     </p>
                                     {{ Illuminate\Support\Str::limit($question->body, 250) }}
